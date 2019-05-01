aee4bf00edb0134fb6a342d2346651670dae67f4
Mateusz Fliszkiewicz <mateusz.fliszkiewicz@gmail.com>
2019-05-01 19:18:04 +0200

    Updating welcome

--- wizWelcome/src/main.ts
+++ wizWelcome/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'zone.js';
 
 if (environment.production) {
   enableProdMode();
